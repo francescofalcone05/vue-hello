9a4253e936c1b56cc460c837a37ff585c1a66403
francescofalcone05 <ciccio_97@live.it>
2024-05-10 18:57:27 +0300

    BONUS

--- js/main.js
+++ js/main.js
@@ -4,20 +4,25 @@ createApp({
   data() {
     return {
       message: 'Benvenuto VueJS',
-      colore: 'red'
+      colore: 'red',
+      immagine: ''
     }
   },
-  methods:{
-    cambiaColore(){
-      
-      if (colore == "red") {
-        colore = "blue"
+  methods: {
+    cambiaColore() {
+      if (this.colore == "red") {
+        this.colore = "blue"
       } else {
-        colore = "red"
+        this.colore = "red"
       }
     },
-    stampaMessaggio(){
-      console.log('messaggio');
+    printImg() {
+      if (this.immagine == '') {
+        this.immagine = './img/01.webp'
+      } else {
+        this.immagine = ''
+      }
+
     }
 
   },
